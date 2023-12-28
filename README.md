e7ac56fd10f4bba12dd822952dd823a2178b7be7
Groove <1178288097@qq.com>
2023-12-28 23:20:15 +0800

    update.

--- README.md
+++ README.md
@@ -7,3 +7,28 @@ run
 ```npm install ```
 
 then double click `index.html` to run.
+
+
+# File & Code Structure
+```
+.
+├── README.md
+├── asset
+│   ├── Round.svg
+│   └── logo.svg
+├── bluetoothTEST.html
+├── index.html
+├── js
+│   ├── STRUCTURE.md
+│   ├── animate.js
+│   ├── createAxes.js
+│   ├── createLight.js
+│   ├── createRay.js
+│   ├── createSphere.js
+│   └── main.js
+├── node_modules
+│   └── three
+├── package-lock.json
+├── package.json
+└── style.css
+```
--- js/main.js
+++ js/main.js
@@ -1,31 +1,9 @@
-//替换three路径为相对路径
-import * as THREE from '/node_modules/three/build/three.module.js';
-import { createAxes } from './createAxes.js';
-import { createRay } from './createRay.js';
-import { animate } from './animate.js';
-import { createSphere } from './createSphere.js';
-import { createLight} from "./createLight.js";
-// 创建场景
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffffff);
+/**
+ * File Name: main.js
+ * Description: Main Program Entry, Initiating Bluetooth Service and 3D Rendering Service.
+ *
+ * Author: GrooveWJH
+ *
+ * Version: 1.0
+ */
 
-// 创建相机
-const camera =
-    new THREE.PerspectiveCamera(50, 1920 / 1080, 0.1, 2000);
-camera.position.set(15, 15, 15);
-camera.lookAt(0, 0, 0);
-// 创建渲染器并设置高分辨率
-export const renderer = new THREE.WebGLRenderer({ antialias: true });
-//由CSS确定大小
-renderer.setSize(1920, 1080);
-renderer.setPixelRatio(window.devicePixelRatio);
-document.body.appendChild(renderer.domElement);
-
-// 添加到场景
-createRay(scene);
-createAxes(scene);
-createSphere(scene);
-createLight(scene);
-
-// 启动动画循环
-animate(renderer, scene, camera);
